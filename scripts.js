5adc7a2d6aacc9963b64cdee1e26c041c896f2ed
Ana Paula Berto <ana.berto@academico.ifpb.edu.br>
2022-02-07 16:29:51 -0300

    commit

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function clicar(){
+    var resenha = document.getElementsByClassName("cabecalho")
+    resenha.style.background-color; '5px solid #ff4aa4'
+    resenha.style.color = 'black'
+}
+
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.moveTo(0, 0);
+ctx.lineTo(200, 100);
+ctx.stroke();
+
